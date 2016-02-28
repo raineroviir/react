e10308e02b5d0ef941a19afbc2b3a5e6bca89229
rainer oviir <raineroviir@rainers-MacBook-Pro.local>
2016-02-28 11:35:59 -0800

    concise predicate and shorten the pathing

--- src/isomorphic/devtools/ReactInvalidSetStateWarningDevTool.js
+++ src/isomorphic/devtools/ReactInvalidSetStateWarningDevTool.js
@@ -7,11 +7,8 @@ if (__DEV__) {
   var processingChildContext = false;
 
   var warnInvalidSetState = function() {
-    if (processingChildContext === false) {
-      return;
-    }
     warning(
-      false,
+      !processingChildContext,
       'setState(...): Cannot call setState inside getChildContext()'
     );
   };
